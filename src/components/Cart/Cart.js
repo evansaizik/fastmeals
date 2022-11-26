192e4ccdea4f216c1955306b8c2966b8db265b9d
isaiah usiholo <evansaizik@gmail.com>
2022-11-26 09:41:20 +0100

    added a checkout component

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,5 +1,5 @@
-import { useContext } from 'react';
-
+import { useContext, useState } from 'react';
+import Checkout from './Checkout';
 import CartContext from '../../store/cart-context';
 import Modal from '../UI/Modal';
 import classes from './Cart.module.css';
@@ -7,6 +7,7 @@ import CartItem from './CartItem';
 
 const Cart = props => {
   const cartCtx = useContext(CartContext);
+  const [isCheckout, setIsCheckout] = useState(false);
   
   const totalAmount = `$${cartCtx.totalAmount.toFixed(2)}`;
   const cartlength = cartCtx.items.length;
@@ -17,6 +18,10 @@ const Cart = props => {
 
   const cartItemAddHandler = item => {
     cartCtx.addItem({...item, amount: 1});
+  };
+
+  const orderHandler = () => {
+    setIsCheckout(true);
   };
 
   const cartItems = <ul className={classes['cart-items']}>{cartCtx.items.map((item) => {
@@ -30,17 +35,22 @@ const Cart = props => {
     />
   })}
   </ul>
+
+  const modalActions = (
+    <div className={classes.actions}>
+      <button onClick={props.onClick} className={classes['button--alt']}>Close</button>
+      {cartlength > 0 && <button className={classes.button} onClick={orderHandler}>Order</button>}
+    </div>
+  )
   
   return <Modal onClick={props.onClick}>
-    {cartItems}
+    {!isCheckout && cartItems}
     <div className={classes.total}>
       <span>Total Amount</span>
     </div>
     <div>{totalAmount}</div>
-    <div className={classes.actions}>
-      <button onClick={props.onClick} className={classes['button--alt']}>Close</button>
-      {cartlength > 0 && <button className={classes.button}>Order</button>}
-    </div>
+    {isCheckout && <Checkout />}
+    {!isCheckout && modalActions}
   </Modal>
 };
 
--- src/components/Meals/AvailableMeals.js
+++ src/components/Meals/AvailableMeals.js
@@ -1,36 +1,60 @@
 import classes from './AvailableMeals.module.css';
 import Card from '../UI/Card';
 import MealItem from './MealItem/MealItem';
-
-const DUMMY_MEALS = [
-  {
-    id: 'm1',
-    name: 'Sushi',
-    description: 'Finest fish and veggies',
-    price: 22.99,
-  },
-  {
-    id: 'm2',
-    name: 'Schnitzel',
-    description: 'A german specialty!',
-    price: 16.5,
-  },
-  {
-    id: 'm3',
-    name: 'Barbecue Burger',
-    description: 'American, raw, meaty',
-    price: 12.99,
-  },
-  {
-    id: 'm4',
-    name: 'Green Bowl',
-    description: 'Healthy...and green...',
-    price: 18.99,
-  },
-];
+import { useEffect, useState } from 'react';
 
 const AvailableMeals = () => {
-  const mealsList = DUMMY_MEALS.map(meal => {
+  const [meals, setMeals] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    
+    const fetchMeals = async () => {
+      try {
+        const response = await fetch('https://food-app-f4d03-default-rtdb.firebaseio.com/meals.json');
+  
+        const responseData = await response.json();
+        console.log(responseData);
+        if (!response.ok) {
+          throw new Error('Something went wrong!')
+        }
+        const loadedMeals = [];
+        for (const key in responseData) {
+          loadedMeals.push({
+            id: key,
+            name: responseData[key].name,
+            description: responseData[key].description,
+            price: responseData[key].price,
+          });
+        };
+  
+        setMeals(loadedMeals);
+        
+      } catch (error) {
+        setError(error.message);
+      }
+    };
+    // fetchMeals().catch((error) => {
+    //   setError(error.message)
+    // });
+    fetchMeals();
+    setIsLoading(false);
+  }, [isLoading]);
+
+  if (isLoading) { 
+    return <section className={classes.MealsLoading}>
+      <p>Loading Meals...</p>
+    </section>
+  }
+
+  if (error) {
+    return <section className={classes.MealsLoading}>
+      <p className={classes.mealsError}>{error}</p>
+    </section>
+  }
+
+  const mealsList = meals.map(meal => {
     return <MealItem 
       key={meal.id} 
       id={meal.id}
@@ -40,11 +64,13 @@ const AvailableMeals = () => {
     />
   });
 
-  return <section className={classes.meals}>
-    <Card>
-      <ul>{mealsList}</ul>
-    </Card>
-  </section>
+  return (
+    <section className={classes.meals}>
+      <Card>
+        <ul>{mealsList}</ul>
+      </Card>
+    </section>
+  )
 };
 
 export default AvailableMeals;
